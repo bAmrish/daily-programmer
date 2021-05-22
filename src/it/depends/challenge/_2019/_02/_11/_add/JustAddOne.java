713b7590e13bc4d41ee50fba5cc252cc57eab900
Amrish Bharatiya <bamrish@gmail.com>
2021-05-22 09:18:44 +0530

    [JustAddOne] Initial template for JustAddOne.

---
+++ src/it/depends/challenge/_2019/_02/_11/_add/JustAddOne.java
@@ -0,0 +1,37 @@
+package it.depends.challenge._2019._02._11._add;
+
+/**
+ * <h1>[2019-02-11] Challenge #375 [Easy] Print a new number by adding one to each of its digit
+ * Description</h1>
+ * <p>
+ * This challenge was originally posted here: <br>
+ * <p>https://www.reddit.com/r/dailyprogrammer/comments/aphavc/20190211_challenge_375_easy_print_a_new_number_by/
+ *
+ * <p>
+ * A number is input in computer then a new no should get printed by adding one to each of its digit.
+ * If you encounter a 9, insert a 10 (don't carry over, just shift things around).
+ * </p>
+ *
+ * <p>
+ * For example, {@code 998} becomes {@code 10109}.
+ * </p>
+ * <p>
+ * <h2>Bonus</h2>
+ * </p>
+ * <p>
+ * This challenge is trivial to do if you map it to a string to iterate over the input, operate,
+ * and then cast it back. Instead, try doing it without casting it as a string at any point,
+ * keep it numeric (int, float if you need it) only.
+ *
+ * </p>
+ * <p>
+ * <h2>Credit</h2>
+ * This challenge was suggested by user /u/chetvishal, many thanks!
+ * If you have a challenge idea please share it in r/dailyprogrammer_ideas
+ * and there's a good chance we'll use it.
+ * </p>
+ */
+public class JustAddOne {
+    public static void main(String[] args) {
+    }
+}
